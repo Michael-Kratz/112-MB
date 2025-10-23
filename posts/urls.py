ce7a1ccec6ebe3e9ad7174b3cb93a13c4587c6c1
Michael-Kratz <Michael.Kratz@sdgku.edu>
2025-10-23 05:50:30 -0700

    final assignment

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 urlpatterns = [
-    path('', PostListView.as_view(), name="posts"),
-    path('detail/', PostDetailView.as_view(), name="detail"),
-    
+    path("", PostListView.as_view(), name="posts_list"),
+    path("detail/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
+    path("new/", PostCreateView.as_view(), name="post_new"),
+    path("edit/<int:pk>/", PostUpdateView.as_view(), name="post_edit"),
+    path("delete/<int:pk>/", PostDeleteView.as_view(), name="post_delete"),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
 
 
 # Create your views here.
@@ -18,6 +20,39 @@ class PostListView(ListView):
 
 
 class PostDetailView(DetailView):
+    """
+    PostDetailView Class is going to retriews a simple element from the Post table in the db
+    """
     template_name = "posts/detail.html"
     model = Post
     context_object_name = "detailed"
+
+class PostCreateView(CreateView):
+    """
+    PostCreateView Class help us to create a new post object
+    """
+    template_name ="posts/new.html"
+    model = Post
+    fields = ["title", "subtitle", "body"]
+
+    def form_valid(self, form):
+        print(form)
+        print(User.objects.all())
+        form.instance.author = User.objects.filter(is_superuser=True).first()
+        return super().form_valid(form)
+    
+class PostUpdateView(UpdateView):
+    """
+    PostUpdateView Class allow us to edit existing records from the db
+    """
+    template_name = "posts/edit.html"
+    model = Post
+    fields = ["title", "subtitle", "body"]
+
+class PostDeleteView(DeleteView):
+    """
+    PostDeleteView Class allow us to delete an existing record from the db
+    """
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy("posts_list")
